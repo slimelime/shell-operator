1539f5d5328472fc766a54eded48aac1745aefd2
pvdvreede <pvdvreede@gmail.com>
2018-09-20 13:21:13 +1000

    Add basic shell execution for reconciliation.

--- controller.go
+++ controller.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"strings"
+	"os"
+	"os/exec"
 	"log"
+	"fmt"
 
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -10,13 +14,35 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 )
 
+const (
+	NameEnvVarKey = "SHOP_OBJECT_NAME"
+	NamespaceEnvVarKey = "SHOP_OBJECT_NAMESPACE"
+)
+
 type ShellReconciler struct {
 	Command string
 }
 
 func (s *ShellReconciler) Reconcile(req reconcile.Request) (reconcile.Result, error) {
-	log.Printf("Reconcile %v\n", req)
-	return reconcile.Result{}, nil
+	if err := runCommandForReconcile(s.Command, req.Name, req.Namespace); err != nil {
+		log.Println("Error", err)
+		return reconcile.Result{Requeue: true}, nil
+	}
+
+	return reconcile.Result{Requeue: false}, nil
+}
+
+func runCommandForReconcile(shellCommand, name, namespace string) error {
+	cmds := strings.Split(shellCommand, " ")
+	cmd := exec.Command(cmds[0], cmds[1:]...)
+	cmd.Env = append(os.Environ(),
+		fmt.Sprintf("%s=%s", NameEnvVarKey, name),
+		fmt.Sprintf("%s=%s", NamespaceEnvVarKey, namespace),
+	)
+	out, err := cmd.CombinedOutput()
+
+	log.Println("output:", string(out))
+	return err
 }
 
 // SetupWatches takes the kubebuilder manager and will setup a kubebuilder controller for
